2cec83af221c3e069c7c9f75e824b144dc8e393f
gedrocht <cookiegedrocht@gmail.com>
2015-01-16 20:08:08 -0500

    steam time update

--- update_steam.bat
+++ update_steam.bat
@@ -6,10 +6,10 @@ python cheevos_do.py shaymus22
 python listTags.py shaymus22
 del steam_shaymus22.py
 copy data_shaymus22.py steam_shaymus22.py
-python ratings_query.py shaymus22
-python steam_ratings.py shaymus22
-python ratings_query.py shaymus22
-python ratings_analyze.py shaymus22
+REM python ratings_query.py shaymus22
+REM python steam_ratings.py shaymus22
+REM python ratings_query.py shaymus22
+REM python ratings_analyze.py shaymus22
 python steam_time.py
 python steam_time_convert_to_js.py
 echo on
